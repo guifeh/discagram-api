b4397bb5126eef0be8726026071e2dcf41979171
guifeh <guigasparinofernandes@gmail.com>
2026-08-28 21:35:05 -0300

    fix: ajuste de program.cs para compilar

--- Entities/User.cs
+++ Entities/User.cs
@@ -3,15 +3,20 @@
 public class User
 {
     public Guid Id { get; set; }
-    
-    public string Name { get; set; }
-    
-    public string Email { get; set; }
-    
+
+    required public string Name { get; set; }
+
+    required public string Email { get; set; }
+
+    required public string UserName { get; set; }
+
+    public bool EmailConfirmed { get; set; }
+
     public string? Phone { get; set; }
-    
-    public string UserName { get; set; }
-    
+
     public byte[] PasswordHash { get; set; } = Array.Empty<byte>();
-    public byte[] PasswordSalt { get; set; }  = Array.Empty<byte>();
+    public byte[] PasswordSalt { get; set; } = Array.Empty<byte>();
+
+    public User() { }
+
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddScoped(IPasswordHash, Criptography)();
+builder.Services.AddScoped<IPasswordHash, Criptography>();
 
 var app = builder.Build();
 
